52e1a5a313c59249dbbd25d3a87488168dbe4c2a
Scott Regenthal <scottregenthal@gmail.com>
2020-01-08 22:04:18 -0700

    Add pexel api service

---
+++ services/pexel_service.rb
@@ -0,0 +1,26 @@
+require 'faraday'
+require 'figaro'
+require 'dotenv'
+Dotenv.load
+
+class PexelService
+  def self.get_photo(pref)
+    json = self.get_json(pref)
+    { url: json[:photos].sample[:src][:large] }
+  end
+
+  private
+
+    def self.connection(pref)
+      Faraday.new(url: 'https://api.pexels.com/v1/search?') do |f|
+        f.params[:query] = pref
+        f.headers[:Authorization] = ENV['PEXEL_API_KEY']
+        f.adapter Faraday.default_adapter
+      end
+    end
+
+    def self.get_json(pref)
+      response = self.connection(pref).get
+      JSON.parse(response.body, symbolize_names: true)
+    end
+end
