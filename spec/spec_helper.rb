185cf054f1da699c9653f4243273f2afeb3d3931
Scott Regenthal <scottregenthal@gmail.com>
2020-01-08 21:58:38 -0700

    Install gems; update ruby version and spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,32 @@
-ENV["RACK_ENV"] = "test"
-require 'bundler'
-require 'simplecov'
-require 'capybara/dsl'
+require 'rack/test'
+require 'rspec'
+require 'pry'
+require 'vcr'
+require 'webmock/rspec'
+require 'shoulda/matchers'
+require File.expand_path '../../routes.rb', __FILE__
 
-Bundler.require(:default, :test)
-require File.expand_path('../../config/environment.rb', __FILE__)
-SimpleCov.start
+ENV['RACK_ENV'] = 'test'
 
-Capybara.app = PenpalApi
-Capybara.save_path = 'tmp/capybara'
+module RSpecMixin
+  include Rack::Test::Methods
+  def app() Sinatra::Application end
+end
 
-RSpec.configure do |c|
-  c.include Capybara::DSL
+RSpec.configure { |c| c.include RSpecMixin }
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+  end
+end
+
+VCR.configure do |config|
+  config.ignore_localhost = true
+  config.cassette_library_dir = 'spec/cassettes'
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+  # config.default_cassette_options = { :match_requests_on => [:method,
+  #   VCR.request_matchers.uri_without_param(:access_token, :apikey)] }
+  config.filter_sensitive_data("<PEXEL_API_KEY>") { ENV['PEXEL_API_KEY'] }
+  config.filter_sensitive_data("<SPOTIFY_ACCESS_TOKEN>") { ENV['SPOTIFY_ACCESS_TOKEN'] }
 end
